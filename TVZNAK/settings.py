1dc90603eec79a5a304307b73d68fd225a4c824f
andreev-sergey <andreevsergeyvrn@gmail.com>
2020-08-25 00:20:39 +0300

    sec

--- TVZNAK/settings.py
+++ TVZNAK/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i*zdz98y%5ml()loq3md^qj5&vq0@e$eb4)vmn%(=+johopac3'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
